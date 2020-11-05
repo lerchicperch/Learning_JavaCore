aa6a6302dfe389c98c41df851263ec12a26318e7
Админ <Lerabananos@mail.ru>
2020-11-05 05:25:23 +0300

    First version of homework for lesson 7

---
+++ src/main/java/ru/geekbrains/lesson7/Apple.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.lesson7;
+
+public class Apple extends Fruit {
+
+    @Override
+    public double getWeight() {
+        return 1.5;
+    }
+}
---
+++ src/main/java/ru/geekbrains/lesson7/Fruit.java
@@ -0,0 +1,6 @@
+package ru.geekbrains.lesson7;
+
+public abstract class Fruit {
+
+    public abstract double getWeight();
+}
